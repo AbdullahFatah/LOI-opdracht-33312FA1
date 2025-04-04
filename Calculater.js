7d669bd6f4f113968317ab5ed7676946e074fbc4
AbdullahFatah <99193897+AbdullahFatah@users.noreply.github.com>
2025-04-04 10:09:49 +0200

    new files added

---
+++ Calculater.js
@@ -0,0 +1,20 @@
+// CALCULATOR PROGRAM
+
+/* Just for fun :) */
+function fun() {
+  display.value = 'Bye bye Abdullah'
+}
+
+/* show the input in display */
+function appendToDisplay(input) {
+  display.value += input
+}
+
+/* calculate the inputs*/
+function calculate() {
+  display.value = eval(display.value)
+}
+/*clear the values*/
+function clearDisplay() {
+  display.value = ''
+}
